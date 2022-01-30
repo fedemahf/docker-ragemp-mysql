d0fb3605a024e1d7693e1ad27fa90708da876b2f
N. Federico Mahfoud <federicomahfoud@gmail.com>
2022-01-30 00:49:20 -0300

    new mysql & ragemp container using docker-compose

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o xtrace
-docker build -t ragemp .
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run \
-	-d --rm \
-	--name ragemp \
-	-p 22005:22005/udp \
-	-p 22006:22006 \
-	ragemp
